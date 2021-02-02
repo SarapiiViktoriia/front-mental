c9b2f9055a84a3081b405dbc7e4dfd254292960a
Viktoriia <viktoriia.sarapii@devit-team.com>
2021-02-02 06:22:38 +0000

    Add enzyme packages

--- src/test/appTest.js
+++ src/test/appTest.js
@@ -1,5 +1,8 @@
 const assert = require('assert');
 const forTest = require('../forTest');
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+configure({ adapter: new Adapter() });
 describe('forTest.sayHello()', function(){
     it('should return Hello.', function(){
         let result = forTest.sayHello()
