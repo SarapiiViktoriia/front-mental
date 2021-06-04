f4df2a689e66e1ab1ee3cdae7db697966317346e
Viktoriia <viktoriia.sarapii@devit-team.com>
2021-06-04 23:05:47 +0000

    twitter is done for charities

--- src/custom.js
+++ src/custom.js
@@ -3,7 +3,6 @@ import React, { Component } from 'react';
 import { Button, Tile, Tab, Tabs, ClickableTile, FormLabel, DataTable, Search, MultiSelect } from "carbon-components-react";
 import { BrowserRouter as Router, Link } from 'react-router-dom';
 const { TableContainer, Table, TableHead, TableRow, TableBody, TableCell, TableHeader } = DataTable;
-var Highlight = require('react-highlighter');
 export const modalProps = () => ({
   id: 'input-modal',
   primaryButtonText: 'Apply Filters',
@@ -88,7 +87,7 @@ export class Card extends Component {
               className="title"
               style={{ "marginTop": "20px", fontSize: "1.2rem" }}
             >
-              <Highlight search={this.props.searchValue}>{this.props.title}</Highlight>
+              {this.props.title}
             </FormLabel>
           </center>
           <br />
@@ -114,7 +113,7 @@ export class Card extends Component {
                   {this.props.label1_heading !== undefined
                     ? this.props.label1_heading + ":"
                     : ""}{" "}
-                  <Highlight search={this.props.searchValue}>{this.props.label1}</Highlight>
+                  {this.props.label1}
                 </FormLabel>
               </center>
             )}
